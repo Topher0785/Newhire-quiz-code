ac8c499a25aded4b7368a430f288dd50871b5d6a
christopher dale <csethdale@live.com>
2021-12-04 14:44:00 -0600

    mostly finished with html and a few css added

---
+++ style/script.js
@@ -0,0 +1,34 @@
+// Variables
+
+
+
+// Begining of quiz
+
+
+
+
+// Timer
+
+
+
+// Answer section is it right or wrong?
+
+
+
+// "the price is WRONG BOB" what will happen now?
+
+// wrong display/text
+
+
+
+
+//questions
+
+
+
+// End results
+
+
+
+
+// Top Score
